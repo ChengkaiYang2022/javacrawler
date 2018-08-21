ab59e79c70b091cb17428b859ca1cb8eb2cc1f92
forrestlmj <18810276930@163.com>
2018-08-21 15:18:32 +0800

    测试多个链接抓取结果

--- src/main/java/FinalDownloader.java
+++ src/main/java/FinalDownloader.java
@@ -1,18 +1,13 @@
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIUtils;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.protocol.HttpCoreContext;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Task;
@@ -20,9 +15,6 @@ import us.codecraft.webmagic.downloader.*;
 import us.codecraft.webmagic.proxy.Proxy;
 import us.codecraft.webmagic.proxy.ProxyProvider;
 import us.codecraft.webmagic.selector.PlainText;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -48,8 +40,9 @@ public class FinalDownloader extends HttpClientDownloader {
         Proxy proxy = proxyProvider != null ? proxyProvider.getProxy(task) : null;
 
         HttpClientRequestContext requestContext = httpUriRequestConverter.convert(request, task.getSite(), proxy);
-        String re = this.getFinalHTTPLocation(requestContext);
-        page.setUrl(new PlainText("http://stockpage.10jqka.com.cn/300179/"));
+        //String re = this.getRedirectUrl(page.getUrl().toString());
+        // String re = this.getFinalHTTPLocation(requestContext);
+        //page.setUrl(new PlainText(re));
         page.setRequest(request);
         page.setStatusCode(200);
         return page;
@@ -76,29 +69,27 @@ public class FinalDownloader extends HttpClientDownloader {
     }
 
     //使用httpGet方法 https://blog.csdn.net/u010443481/article/details/44811381
-    public void getRedirectInfo(String url){
+    public String getRedirectUrl(String url){
         HttpClient httpClient = new DefaultHttpClient();
         HttpContext httpContext = new BasicHttpContext();
         HttpGet httpGet = new HttpGet(url);
+        String redirectUrl = null;
         try {
             //将HttpContext对象作为参数传给execute()方法,则HttpClient会把请求响应交互过程中的状态信息存储在HttpContext中
             HttpResponse response = httpClient.execute(httpGet, httpContext);
-            //获取重定向之后的主机地址信息,即"http://127.0.0.1:8088"
-            HttpHost targetHost = (HttpHost)httpContext.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
-            //获取实际的请求对象的URI,即重定向之后的"/blog/admin/login.jsp"
-            HttpUriRequest realRequest = (HttpUriRequest)httpContext.getAttribute(ExecutionContext.HTTP_REQUEST);
-            System.out.println("主机地址:" + targetHost);
-            System.out.println("URI信息:" + realRequest.getURI());
-            HttpEntity entity = response.getEntity();
-            if(null != entity){
-                System.out.println("响应内容:" + EntityUtils.toString(entity, ContentType.getOrDefault(entity).getCharset()));
-                EntityUtils.consume(entity);
-            }
+            //获取重定向之后的主机地址信息
+            Object a = httpContext.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
+            HttpHost targetHost = (HttpHost)httpContext.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
+            //获取实际的请求对象的URI
+            HttpUriRequest realRequest = (HttpUriRequest)httpContext.getAttribute(HttpCoreContext.HTTP_REQUEST);
+            redirectUrl = targetHost.toString() + realRequest.getURI().toString();
+            // System.out.println(redirectUrl);
         } catch (Exception e) {
             e.printStackTrace();
         }finally{
             httpClient.getConnectionManager().shutdown();
         }
+        return redirectUrl;
     }
 }
 
--- src/test/java/FinalDownloaderTest.java
+++ src/test/java/FinalDownloaderTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import us.codecraft.webmagic.Page;
@@ -6,9 +7,25 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.selector.Html;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FinalDownloaderTest {
 
+    private List<String> testUrl = new ArrayList<String>();
     private String URL = "http://www.baidu.com/link?url=SCZi8UF0UZ_L688dALi_w1gDL5G4G5QYs30-1mv7nDPcaRH-DKymS_5gRnDhJ4MGUGe5SaKOsc_3I4q_IBOIJ_";
+
+    @Before
+    public void init(){
+        String URL_CNBLOG = "http://www.baidu.com/link?url=SCZi8UF0UZ_L688dALi_w1gDL5G4G5QYs30-1mv7nDPcaRH-DKymS_5gRnDhJ4MGUGe5SaKOsc_3I4q_IBOIJ_";
+        String URL_TIANHONG = "http://www.baidu.com/link?url=---33rXjyw861WwuVBNKwS0yaha5l_z0I9tBaJ-_3JMIK98rHB6WWT3EW3g1IshPfr1yD8IGCj1l83arAFAmB2mK4ZVOkFrrMG9c4UzIOMUA8tE8gQwnhtXtFFV6Hfs0";
+        String URL3 = "http://www.baidu.com/link?url=---yJOodqyT2CM0ueRB8pF3fWwYEe8Bb0GQp8rctUtho_Q7N5VnGO9bK8I_4D5rb021bASfA7sIvl6WqjisCaK";
+
+        testUrl.add(URL_CNBLOG);
+        testUrl.add(URL_TIANHONG);
+        testUrl.add(URL3);
+
+    }
 
     @Ignore
     public void testFinalDownloaderTest(){
@@ -21,7 +38,11 @@ public class FinalDownloaderTest {
     @Test
     public void test_getRedirectInfo(){
         FinalDownloader finalDownloader = new FinalDownloader();
-        finalDownloader.getRedirectInfo(URL);
+        for (String url:this.testUrl){
+            String result = finalDownloader.getRedirectUrl(url);
+            System.out.println(result);
+        }
+
     }
     @Ignore
     public void testFinalDownloaderTestBaiduZhidao(){
@@ -31,4 +52,6 @@ public class FinalDownloaderTest {
         Page page = finalDownloader.download(request,task);
         System.out.println(page);
     }
+
+
 }
