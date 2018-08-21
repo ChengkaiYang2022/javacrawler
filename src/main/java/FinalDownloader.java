53854ecf807227ad3a5d9c03f5b9bb21c1dd247b
forrestlmj <18810276930@163.com>
2018-08-21 11:56:32 +0800

    使用httpget成功

--- src/main/java/FinalDownloader.java
+++ src/main/java/FinalDownloader.java
@@ -1,7 +1,18 @@
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Task;
@@ -46,25 +57,50 @@ public class FinalDownloader extends HttpClientDownloader {
     protected String getFinalHTTPLocation(HttpClientRequestContext requestContext) {
         String last_redirect_url = null;
         try {
-            Thread.sleep(5000);
 
             HttpHost target = requestContext.getHttpClientContext().getTargetHost();
-            Thread.sleep(5000);
             //https://github.com/code4craft/webmagic/issues/626
 
             //TODO 这里是空的
             List redirectLocations = requestContext.getHttpClientContext().getRedirectLocations();
-            Thread.sleep(5000);
+            //Thread.sleep(5000);
 
             URI location = URIUtils.resolve(requestContext.getHttpUriRequest().getURI(), target, redirectLocations);
             //logger.debug("Final HTTP location: " + location.toASCIIString());
             last_redirect_url = location.toASCIIString();
+            System.out.println(last_redirect_url);
         }catch (URISyntaxException e) {
-            //logger.error("getFinalHTTPLocation error",e);
-        }catch (InterruptedException e) {
-            e.printStackTrace();
+            logger.error("getFinalHTTPLocation error",e);
         }
         return last_redirect_url;
     }
 
+    //使用httpGet方法 https://blog.csdn.net/u010443481/article/details/44811381
+    public void getRedirectInfo(String url){
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpContext httpContext = new BasicHttpContext();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            //将HttpContext对象作为参数传给execute()方法,则HttpClient会把请求响应交互过程中的状态信息存储在HttpContext中
+            HttpResponse response = httpClient.execute(httpGet, httpContext);
+            //获取重定向之后的主机地址信息,即"http://127.0.0.1:8088"
+            HttpHost targetHost = (HttpHost)httpContext.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+            //获取实际的请求对象的URI,即重定向之后的"/blog/admin/login.jsp"
+            HttpUriRequest realRequest = (HttpUriRequest)httpContext.getAttribute(ExecutionContext.HTTP_REQUEST);
+            System.out.println("主机地址:" + targetHost);
+            System.out.println("URI信息:" + realRequest.getURI());
+            HttpEntity entity = response.getEntity();
+            if(null != entity){
+                System.out.println("响应内容:" + EntityUtils.toString(entity, ContentType.getOrDefault(entity).getCharset()));
+                EntityUtils.consume(entity);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            httpClient.getConnectionManager().shutdown();
+        }
+    }
 }
+
+
+
--- src/test/java/FinalDownloaderTest.java
+++ src/test/java/FinalDownloaderTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
@@ -6,9 +7,25 @@ import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.selector.Html;
 
 public class FinalDownloaderTest {
-    @Test
+
+    private String URL = "http://www.baidu.com/link?url=SCZi8UF0UZ_L688dALi_w1gDL5G4G5QYs30-1mv7nDPcaRH-DKymS_5gRnDhJ4MGUGe5SaKOsc_3I4q_IBOIJ_";
+
+    @Ignore
     public void testFinalDownloaderTest(){
-        Request request = new Request("http://www.baidu.com/link?url=SCZi8UF0UZ_L688dALi_w1gDL5G4G5QYs30-1mv7nDPcaRH-DKymS_5gRnDhJ4MGUGe5SaKOsc_3I4q_IBOIJ_");
+        Request request = new Request(URL);
+        Task task = Site.me().setDomain("localhost").setCycleRetryTimes(5).toTask();
+        FinalDownloader finalDownloader = new FinalDownloader();
+        Page page = finalDownloader.download(request,task);
+        System.out.println(page);
+    }
+    @Test
+    public void test_getRedirectInfo(){
+        FinalDownloader finalDownloader = new FinalDownloader();
+        finalDownloader.getRedirectInfo(URL);
+    }
+    @Ignore
+    public void testFinalDownloaderTestBaiduZhidao(){
+        Request request = new Request("http://baike.baidu.com/subview/38681/5279942.htm");
         Task task = Site.me().setDomain("localhost").setCycleRetryTimes(5).toTask();
         FinalDownloader finalDownloader = new FinalDownloader();
         Page page = finalDownloader.download(request,task);
