18408ecd448672d62c265ce7e19ce5b7bffcd5f2
forrestlmj <18810276930@163.com>
2018-08-23 17:06:20 +0800

    kafka feature

--- src/main/java/GithubRepoPageProcessor.java
+++ src/main/java/GithubRepoPageProcessor.java
@@ -1,4 +1,5 @@
 import org.apache.log4j.BasicConfigurator;
+import scheduler.KafkaConsumerScheduler;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -25,20 +26,20 @@ public class GithubRepoPageProcessor implements PageProcessor {
         BasicConfigurator.configure();
 
         Spider spider = new Spider(new GithubRepoPageProcessor());
-
-        while(1==1){
-            spider.thread(5);
-            spider.run();
-            spider.setDownloader(new RedirectDownloader());
-            spider.addUrl("http://www.baidu.com/link?url=SCZi8UF0UZ_L688dALi_w1gDL5G4G5QYs30-1mv7nDPcaRH-DKymS_5gRnDhJ4MGUGe5SaKOsc_3I4q_IBOIJ_");
-            String URL3 = "http://www.baidu.com/link?url=---yJOodqyT2CM0ueRB8pF3fWwYEe8Bb0GQp8rctUtho_Q7N5VnGO9bK8I_4D5rb021bASfA7sIvl6WqjisCaK";
-            spider.addUrl(URL3);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        spider.setScheduler(new KafkaConsumerScheduler()).thread(5).run();
+//        while(1==1){
+//            spider.thread(5);
+//            spider.run();
+//            //spider.setDownloader(new RedirectDownloader());
+//            spider.addUrl("http://www.baidu.com/link?url=SCZi8UF0UZ_L688dALi_w1gDL5G4G5QYs30-1mv7nDPcaRH-DKymS_5gRnDhJ4MGUGe5SaKOsc_3I4q_IBOIJ_");
+//            String URL3 = "http://www.baidu.com/link?url=---yJOodqyT2CM0ueRB8pF3fWwYEe8Bb0GQp8rctUtho_Q7N5VnGO9bK8I_4D5rb021bASfA7sIvl6WqjisCaK";
+//            spider.addUrl(URL3);
+//            try {
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
     }
 }
--- src/main/java/scheduler/KafkaConsumerScheduler.java
+++ src/main/java/scheduler/KafkaConsumerScheduler.java
@@ -1,4 +1,77 @@
 package scheduler;
 
-public class KafkaConsumerScheduler {
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.scheduler.DuplicateRemovedScheduler;
+import us.codecraft.webmagic.scheduler.MonitorableScheduler;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class KafkaConsumerScheduler extends DuplicateRemovedScheduler implements MonitorableScheduler {
+
+    public static final int KAFKA_QUEUE_SIZE = 3;
+    private BlockingQueue<Request> kafkaQueue = new LinkedBlockingDeque<Request>();
+
+    public KafkaConsumerScheduler()  {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "blackbox01.jry.com:6667");
+        properties.put("group.id", "CRAWLER_JAVA");
+        properties.put("enable.auto.commit", "true");
+        properties.put("auto.commit.interval.ms", "1000");
+        properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        consumer.subscribe(Arrays.asList("link_test"));
+        while (true) {
+
+            if(kafkaQueue.size() < KAFKA_QUEUE_SIZE){
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(record);
+                    Request request = new Request();
+                    request.setUrl("http://futures.jrj.com.cn/gzqh/2016/06/20213521091327.shtml");
+                    kafkaQueue.add(request);
+                }
+            }else{
+                try{
+                    Thread.sleep(200);
+                    System.out.println("wait" + kafkaQueue.size());
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+    @Override
+    protected void pushWhenNoDuplicate(Request request, Task task) {
+        kafkaQueue.add(request);
+    }
+
+    @Override
+    public synchronized Request poll(Task task) {
+        return kafkaQueue.poll();
+    }
+
+
+    @Override
+    public int getLeftRequestsCount(Task task) {
+        return kafkaQueue.size();
+    }
+
+    @Override
+    public int getTotalRequestsCount(Task task) {
+        return getDuplicateRemover().getTotalRequestsCount(task);
+    }
 }
